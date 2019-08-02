5186ac65fb5029e38e600608aef8ae99bfae4a4b
onewoorks <irwanbibrahim@onewoorks.com>
2019-08-03 04:52:20 +0800

    release

--- controller/laporanController.js
+++ controller/laporanController.js
@@ -1,16 +1,36 @@
 angular.module('zakApp').controller('laporanUntungJualanController', ['$scope', '$http',
     function ($scope, $http) {
-        $scope.testlu = "ok proceed"
+        $scope.tarikhMula = ""
+        $scope.tarikhAkhir = ""
         $scope.listEmasJual = []
 
         var getRekodJualanEmas = () => {
             $http.get(api_url + '/laporan/semua-jualan')
                 .then(function (response) {
-                    console.log(response.data.result)
                     $scope.listEmasJual = response.data.result.itemize;
                     $scope.summary = response.data.result.summary;
                 })
         }
-
         getRekodJualanEmas()
+
+        var getRekodJualanEmasTarikh = (tarikh_mula, tarikh_akhir) => {
+            $http({
+                url: api_url + '/laporan/semua-jualan',
+                method: 'get',
+                params: {
+                    tarikh_mula: dBdate(tarikh_mula),
+                    tarikh_akhir: dBdate(tarikh_akhir)
+                }
+            })
+                .then(function (response) {
+                    $scope.listEmasJual = response.data.result.itemize;
+                    $scope.summary = response.data.result.summary;
+                });
+        }
+
+        $scope.buatCarian = () => {
+            let tarikh_mula = $('[name=tarikh_mula]').val();
+            let tarikh_akhir = $('[name=tarikh_akhir]').val();
+            getRekodJualanEmasTarikh(tarikh_mula, tarikh_akhir)
+        }
     }])
