90cc136572a863e50e54a34762b4154076da9638
onewoorks <irwanbibrahim@onewoorks.com>
2019-08-03 02:44:08 +0800

    update jual emas

--- controller/laporanController.js
+++ controller/laporanController.js
@@ -1,16 +1,16 @@
-angular.module('zakApp').controller('laporanUntungJualanController',['$scope','$http', 
-function($scope,$http){
-    $scope.testlu = "ok proceed"
+angular.module('zakApp').controller('laporanUntungJualanController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.testlu = "ok proceed"
+        $scope.listEmasJual = []
 
-    var getRekodJualanEmas = () => {
-        $http.get(api_url + '/laporan/semua-jualan')
-                    .then(function (response) {
-                        $scope.listCawanganStok = response.data.result.itemize;
-                        $scope.summary = response.data.result.summary;
-                        for (i = 0; i < $scope.listCawanganStok.length; i++) {
-                            $scope.berat_nak_jual[i] = $scope.listCawanganStok[i].berat_stok
-                            $scope.jual_item[i] = true
-                        }
-                    })
-    }
-}])
+        var getRekodJualanEmas = () => {
+            $http.get(api_url + '/laporan/semua-jualan')
+                .then(function (response) {
+                    console.log(response.data.result)
+                    $scope.listEmasJual = response.data.result.itemize;
+                    $scope.summary = response.data.result.summary;
+                })
+        }
+
+        getRekodJualanEmas()
+    }])
